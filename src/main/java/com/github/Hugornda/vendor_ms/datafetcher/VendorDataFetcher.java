8191dc44b1e4ec90d82d82a03d814e6dd33ae053
hugornda <hugornda@gmail.com>
2024-08-26 11:58:42 +0100

    feat: added log to get all vendors query handling

--- src/main/java/com/github/Hugornda/vendor_ms/datafetcher/VendorDataFetcher.java
+++ src/main/java/com/github/Hugornda/vendor_ms/datafetcher/VendorDataFetcher.java
@@ -4,11 +4,16 @@ import com.github.Hugornda.vendor_ms.model.Vendor;
 import com.github.Hugornda.vendor_ms.repository.VendorRepository;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsQuery;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
+
+import static com.github.Hugornda.vendor_ms.utils.FormatterUtils.toJsonString;
 
 @DgsComponent
 public class VendorDataFetcher {
 
+    private static final Logger log = LoggerFactory.getLogger(VendorDataFetcher.class);
 
     private final VendorRepository vendorRepository;
 
@@ -18,6 +23,7 @@ public class VendorDataFetcher {
 
     @DgsQuery(field = "vendors")
     public Flux<Vendor> getVendors() {
-        return vendorRepository.findAll();
+        return vendorRepository.findAll()
+                .doOnNext(res->log.info(toJsonString(res)));
     }
 }

8af61b6fdaed70dae5256abb61e8bde05d92aeee
hugornda <hugornda@gmail.com>
2024-08-26 11:49:11 +0100

    feat: added vendor creation mutation

---
+++ src/main/java/com/github/Hugornda/vendor_ms/mutations/VendorMutations.java
@@ -0,0 +1,37 @@
+package com.github.Hugornda.vendor_ms.mutations;
+
+import com.github.Hugornda.vendor_ms.model.Vendor;
+import com.github.Hugornda.vendor_ms.model.exceptions.VendorAlreadyExistsException;
+import com.github.Hugornda.vendor_ms.repository.VendorRepository;
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsMutation;
+import com.netflix.graphql.dgs.InputArgument;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.security.access.prepost.PreAuthorize;
+import reactor.core.publisher.Mono;
+
+import static com.github.Hugornda.vendor_ms.utils.FormatterUtils.toJsonString;
+
+@Slf4j
+@DgsComponent
+public class VendorMutations {
+
+    private final VendorRepository vendorRepository;
+
+    public VendorMutations(VendorRepository vendorRepository) {
+        this.vendorRepository = vendorRepository;
+    }
+
+    @DgsMutation
+    @PreAuthorize("hasRole('ADMIN')")
+    public Mono<Vendor> createVendor(@InputArgument String name,
+                                     @InputArgument Integer numberOfEmployees,
+                                     @InputArgument String country) {
+        Vendor vendor = new Vendor(name, numberOfEmployees, country);
+        return vendorRepository.save(vendor)
+                .doOnNext(res-> log.info("Created vendor: {}", toJsonString(res)))
+                .onErrorResume(DuplicateKeyException.class, e -> Mono.error(
+                        new VendorAlreadyExistsException("A vendor with this name already exists.")));
+    }
+}

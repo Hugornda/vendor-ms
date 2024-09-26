139185e5f5872ad0d6928f34f5d6c789c82e6d2d
hugornda <hugornda@gmail.com>
2024-09-26 15:34:39 +0100

    feat: added filtered vendors by employee number

--- src/main/java/com/github/Hugornda/vendor_ms/repository/VendorRepository.java
+++ src/main/java/com/github/Hugornda/vendor_ms/repository/VendorRepository.java
@@ -2,10 +2,15 @@ package com.github.Hugornda.vendor_ms.repository;
 
 import com.github.Hugornda.vendor_ms.model.Vendor;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 
 public interface VendorRepository  extends ReactiveCrudRepository<Vendor, Long> {
     @NotNull
     Flux<Vendor> findAll();
+
+    @Query("SELECT * FROM Vendors WHERE n_employees < :numEmployees")
+    Flux<Vendor> filteredVendorsByEmployeeNumber(@Param("numEmployees") int numEmployees);
 }
